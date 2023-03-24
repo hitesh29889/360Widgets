00a290a6ca739ecd5c2c34edabc465b4c8cb98f2
Hitesh Asodiya <hitesh.29889@gmail.com>
2023-03-24 19:30:09 +0530

    removed unnecessary code

--- Models/InputModel.cs
+++ Models/InputModel.cs
@@ -5,21 +5,11 @@ using System.Threading.Tasks;
 
 namespace _360Widgets.Models
 {
-    public class InputModel
-    {
-        public string Reference { get; set; }
-        public List<Dictionary<string, string>> Thermometer { get; set; }
-        public List<Dictionary<string, string>> Humidity { get; set; }
-        public List<Dictionary<string, string>> Monoxide { get; set; }
-    }
-
-
     public class InputModelText
     { 
         public string Reference { get; set; }
         public List<List<string>> Thermometer { get; set; } = new List<List<string>>();
         public List<List<string>> Humidity { get; set; } = new List<List<string>>();
         public List<List<string>> Monoxide { get; set; } = new List<List<string>>();
-
     }
 }
